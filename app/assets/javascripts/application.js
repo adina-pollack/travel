32d7cc63e6de56a626af84cb8780e40f283537a1
Adina Pollack <adinap16@gmail.com>
2016-08-08 10:45:48 -0400

    working fb login and figaro

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,104 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+window.fbAsyncInit = function() {
+ FB.init({
+   appId      : '{your-app-id}',
+   cookie     : true,  // enable cookies to allow the server to access
+                       // the session
+   xfbml      : true,  // parse social plugins on this page
+   version    : 'v2.5' // use graph api version 2.5
+ });
+ FB.getLoginStatus(function(response) {
+  statusChangeCallback(response);
+});
+};
+ (function(d, s, id) {
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) return;
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+
+// This is called with the results from from FB.getLoginStatus().
+ function statusChangeCallback(response) {
+   console.log('statusChangeCallback');
+   console.log(response);
+   // The response object is returned with a status field that lets the
+   // app know the current login status of the person.
+   // Full docs on the response object can be found in the documentation
+   // for FB.getLoginStatus().
+   if (response.status === 'connected') {
+     // Logged into your app and Facebook.
+     testAPI();
+   } else if (response.status === 'not_authorized') {
+     // The person is logged into Facebook, but not your app.
+     document.getElementById('status').innerHTML = 'Please log ' +
+       'into this app.';
+   } else {
+     // The person is not logged into Facebook, so we're not sure if
+     // they are logged into this app or not.
+     document.getElementById('status').innerHTML = 'Please log ' +
+       'into Facebook.';
+   }
+ }
+
+ // This function is called when someone finishes with the Login
+ // Button.  See the onlogin handler attached to it in the sample
+ // code below.
+ function checkLoginState() {
+   FB.getLoginStatus(function(response) {
+     statusChangeCallback(response);
+   });
+ }
+
+function testAPI() {
+   console.log('Welcome!  Fetching your information.... ');
+   FB.api('/me', function(response) {
+     console.log('Successful login for: ' + response.name);
+     document.getElementById('status').innerHTML =
+       'Thanks for logging in, ' + response.name + '!';
+   });
+ };
+
+
+//
+// $(document).ready(function(){
+//   $.ajaxSetup({ cache: true });
+//   $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+//    FB.init({
+//      appId: '{1002089086577512}',
+//      version: 'v2.7'
+//    });
+//    $('#loginbutton').removeAttr('disabled');
+//    FB.getLoginStatus(updateStatusCallback);
+//  });
+//
+//  function updateStatusCallback(response){
+//    if (response.status === 'connected'){
+//      testAPI();
+//    } else{
+//      $('status').html('Please log into Facebook.');
+//    }
+//  }
+//  function testAPI(){
+//    FB.api('/me', function(response){
+//      $('status').html('Thanks for logging in,' + response.name + '!')
+//    });
+//  }
+// })
---
+++ db/migrate/20160807202024_create_destinations.rb
@@ -0,0 +1,9 @@
+class CreateDestinations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :destinations do |t|
+      t.references :user
+      t.references :location
+      t.timestamps
+    end
+  end
+end
