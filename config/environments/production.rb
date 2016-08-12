627ad600dc2036cfdb4eedd70d1874ebb31b225b
Adina Pollack <adinap16@gmail.com>
2016-08-12 10:47:09 -0400

    cable.yml add actioncable config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.web_socket_server_url = "wss://make-friends-will-travel.herokuapp.com/cable"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://make-friends-will-travel.herokuapp.com', 'http://make-friends-will-travel.herokuapp.com', /http:\/\/make-friends-will-travel.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
